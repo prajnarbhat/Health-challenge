6221549a51661998240bb83be3965d16aa0a88b7
prajnarbhat <rbhatprajna@gmail.com>
2025-04-11 12:18:37 +0530

    Applied pagination

--- src/components/WorkoutTable.js
+++ src/components/WorkoutTable.js
@@ -5,6 +5,11 @@ const WorkoutTable = () => {
     const {data} = useContext(WorkoutContext)
 
     const [userData, setData] = useState([])
+    const [page,setPage] = useState(1)
+
+    const itemsPerPage = 4;
+
+    let paginatedData = userData.slice((itemsPerPage*page) - itemsPerPage,itemsPerPage*page)
 
     console.log("Data in table:", data)
 
@@ -12,7 +17,13 @@ const WorkoutTable = () => {
         setData(data);
     })
 
+    const selectPageHandler = (selectedPage) => {
+        if(selectedPage >= 1 && selectedPage <= Math.ceil(userData.length/itemsPerPage) && selectedPage != page)
+        setPage(selectedPage)
+    }
+
     return (
+        <>
         <table border="1">
             <thead>
                 <tr>
@@ -22,8 +33,8 @@ const WorkoutTable = () => {
                 </tr>
             </thead>
             <tbody>
-                {userData.length > 0 ? (
-                    userData.map((user,index) => (
+                {paginatedData.length > 0 ? (
+                    paginatedData.map((user,index) => (
                         <tr key={index}>
                         <td> {user.userName} </td>
                         <td> {user.workouts.map(workout => workout.workoutType).join(",")}</td>
@@ -41,6 +52,23 @@ const WorkoutTable = () => {
             </tbody>
 
         </table>
+
+        {userData.length > 0 && (
+            <div className="pagination">
+
+                <span className={page > 1 ? "" : "page_disabled"} onClick={() => selectPageHandler(page-1)}> <button> Previous</button></span> 
+          
+                {[...Array(Math.ceil(userData.length/itemsPerPage))].map((__dirname, index) => (
+                    <span onClick={() => selectPageHandler(index+1)} className={page == index+1 ? "page_selected" : ""}> <button> {index + 1} </button></span> ))
+                }
+
+                <span className={page < Math.ceil(userData.length/itemsPerPage) ? "" : "page_disabled"} onClick={() => selectPageHandler(page+1)}><button> Next </button></span>
+
+            </div>
+        )}
+        </>
+
+        
     )
 }
 
