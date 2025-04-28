b87ae63f2b2aa60cd7e6bc4e8fcbff51bffbf28c
prajnarbhat <rbhatprajna@gmail.com>
2025-04-28 13:09:55 +0530

    Navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
                 <div className="container mx-auto flex items-center justify-between px-6 py-3 ml-20">
                     <Link to="/" className="flex items-center space-x-3">
                         <img src={logo} alt="logo" className="h-10 w-10 rounded-full" />
-                        <h3 className="text-xl font-bold hover:text-blue-300 transition-colors duration-200 text-base font-medium"> Crypto Hunter</h3> 
+                        <h3 className="text-xl font-bold hover:text-blue-300 transition-colors duration-200 text-base font-medium"> Health Challenge Tracker</h3> 
                      
                     </Link>
                     <div className="flex space-x-6 mr-20"> 
